cdee685d73311a5c63715d02e1ca160f9ddeb82a
Bradyn <bradyn.parisian@gmail.com>
2021-02-11 19:18:28 +0000

    added movement comments WASD for up, left, down, right

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
+let connection; //stores the active TCP connection object
+
 const { builtinModules } = require("module");
 
 const setupInput = (conn) => { // connects to host server
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -8,10 +11,22 @@ const setupInput = (conn) => { // connects to host server
   process.stdin.on("data", handleUserInput);
   return stdin;
 }
-
+// Note: QWERTY keyboard hex key values for commands
 const handleUserInput = (key) => { //listener for user input to execute game actions
   if (key === '\u0003') { // Ctrl + C equivalent
     process.exit();
+  }
+  if (key === '\0077') { // Up - 'w' key equivalent
+    connection.write("Move: up");
+  }
+  if (key === '\0061') { // Left - 'a' key equivalent
+    connection.write("Move: left");
+  }
+  if (key === '\0073') { // Down - 's' key equivalent
+    connection.write("Move: down");
+  }
+  if (key === '\0064') { // Right - 'd' key equivalent
+    connection.write("Move: right");
   }
 }
 
